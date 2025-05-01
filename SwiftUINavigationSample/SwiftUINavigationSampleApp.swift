a8bf9c0e82e9bc2beee363607bf37f424a4f08a1
Kristanto Sean <kristantosean@gmail.com>
2025-05-01 14:55:04 +0700

    Initial Commit

---
+++ SwiftUINavigationSample/SwiftUINavigationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationSampleApp.swift
+//  SwiftUINavigationSample
+//
+//  Created by Kristanto Sean on 2025-05-01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
